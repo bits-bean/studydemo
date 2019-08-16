a485676cb0665c16e8f33bdb1d356dfc2bc09131
dugm <19911108>
2019-08-17 00:07:03 +0800

    commit

---
+++ src/main/java/com/bugbean/studydemo/juc/Tester.java
@@ -0,0 +1,25 @@
+package com.bugbean.studydemo.juc;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Tester {
+
+    private Lock lock = new ReentrantLock();
+
+    public synchronized void method1() {
+        System.out.println("method1");
+    }
+
+    public void method2() {
+        lock.lock();
+        System.out.println("method2");
+        lock.unlock();
+    }
+
+    public void method3() {
+        synchronized (this) {
+            System.out.println("method3");
+        }
+    }
+}
