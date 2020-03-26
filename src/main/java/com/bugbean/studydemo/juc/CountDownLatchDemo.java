f0e9513e77c0cf382785ae948a68f3b991ba5bcf
dugm <19911108>
2020-03-26 14:37:54 +0800

    commit

--- src/main/java/com/bugbean/studydemo/juc/CountDownLatchDemo.java
+++ src/main/java/com/bugbean/studydemo/juc/CountDownLatchDemo.java
@@ -5,7 +5,7 @@ import java.util.concurrent.CountDownLatch;
 public class CountDownLatchDemo {
 	public static void main(String[] args) throws InterruptedException {
 
-		CountDownLatch countDownLatch = new CountDownLatch(6);
+		CountDownLatch countDownLatch = new CountDownLatch(1);
 
 		for (int i = 0; i < 6; i++) {
 			new Thread(() -> {
